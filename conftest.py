dcc04e1e3d1d57fa12d44853eb2fb87ef2b3d95c
Bruno Alla <alla.brunoo@gmail.com>
2023-08-30 20:55:29 +0000

    chore: add missing docstrings

--- conftest.py
+++ conftest.py
@@ -7,6 +7,7 @@ import pytest
 
 @pytest.fixture
 def workspace(tmp_path: Path) -> Generator[Path, None, None]:
+    """Fixture to change the current directory to a temporary one."""
     cwd = os.getcwd()
     os.chdir(tmp_path)
     yield tmp_path
